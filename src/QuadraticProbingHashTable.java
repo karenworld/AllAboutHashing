35303b5d7f2f17a27af335dab42e3866f79af2d4
nobody <karenworld@yahoo.com>
2024-08-12 13:11:22 +0800

    Using Prime to avoid clustering

--- src/QuadraticProbingHashTable.java
+++ src/QuadraticProbingHashTable.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class QuadraticProbingHashTable {
 	private Integer[] hashTable;
     private int tableSize;
+    private int size;
 
     public QuadraticProbingHashTable(int size) {
         tableSize = size;
@@ -14,23 +15,62 @@ public class QuadraticProbingHashTable {
         return key % tableSize;
     }
     
+    private void resizeTable() {
+        int newSize = findNextPrime(tableSize * 2);
+        Integer[] newHashTable = new Integer[newSize];
+        Arrays.fill(newHashTable, null);
+
+        for (Integer key : hashTable) {
+            if (key != null) {
+                int index = key % newSize;
+                int i = 0;
+                while (newHashTable[(index + i * i) % newSize] != null) {
+                    i++;
+                }
+                newHashTable[(index + i * i) % newSize] = key;
+            }
+        }
+
+        hashTable = newHashTable;
+        tableSize = newSize;
+    }
+//    
+//    public void insert(int key) {
+//        int index = hashFunction(key);
+//        int i = 0;
+//
+//        // Quadratic probing
+//        while (hashTable[(index + i * i) % tableSize] != null) {
+//            i++;
+//            if (i == tableSize) {
+//                // This means the table is full and no empty slot was found
+//                System.out.println("HashTable is full, cannot insert key: " + key);
+//                return;
+//            }
+//        }
+//
+//        // Insert the key at the calculated index
+//        hashTable[(index + i * i) % tableSize] = key;
+//    }
+   // refactored code
     public void insert(int key) {
+        if (size >= tableSize / 2) {
+            resizeTable();
+        }
+
         int index = hashFunction(key);
         int i = 0;
 
         // Quadratic probing
         while (hashTable[(index + i * i) % tableSize] != null) {
             i++;
-            if (i == tableSize) {
-                // This means the table is full and no empty slot was found
-                System.out.println("HashTable is full, cannot insert key: " + key);
-                return;
-            }
         }
 
         // Insert the key at the calculated index
         hashTable[(index + i * i) % tableSize] = key;
+        size++;
     }
+
 
 //    public void insert(int key) {
 //        int index = hashFunction(key);
@@ -55,6 +95,24 @@ public class QuadraticProbingHashTable {
             }
         }
     }
+    
+    private int findNextPrime(int num) {
+        while (!isPrime(num)) {
+            num++;
+        }
+        return num;
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    
 
     public static void main(String[] args) {
         QuadraticProbingHashTable qHashTable = new QuadraticProbingHashTable(11);
@@ -69,6 +127,8 @@ public class QuadraticProbingHashTable {
     }
 }
 
+
+//result without using Prime resulting in clustering
 //HashTable is full, cannot insert key: 88
 //HashTable is full, cannot insert key: 99
 //Index 0: 22
@@ -82,4 +142,29 @@ public class QuadraticProbingHashTable {
 //Index 8: null
 //Index 9: 55
 //Index 10: null
+
+//using Primme
+//Index 0: null
+//Index 1: null
+//Index 2: null
+//Index 3: null
+//Index 4: null
+//Index 5: null
+//Index 6: null
+//Index 7: 99
+//Index 8: 77
+//Index 9: 55
+//Index 10: 33
+//Index 11: null
+//Index 12: null
+//Index 13: null
+//Index 14: null
+//Index 15: null
+//Index 16: null
+//Index 17: null
+//Index 18: null
+//Index 19: 88
+//Index 20: 66
+//Index 21: 44
+//Index 22: 22
 
